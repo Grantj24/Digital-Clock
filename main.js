47d2933f5e154f7ff00b829f7acc67db78f4db32
Grantj24 <jeffriesg15@gmail.com>
2022-09-06 13:29:10 -0400

    changes to javascript and formatting of html/css

--- main.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-var dd = String(today. getDate()). padStart(2,'0');
-var mm = String(today.getMonth() + 1). padStart(2, '0'); 
-var yyyy = today.getFullYear(); 
-
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today); 
