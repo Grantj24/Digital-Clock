abe0308607d45f2ddd87f1a77c4bbf167d49d5bd
Grantj24 <jeffriesg15@gmail.com>
2022-09-09 12:04:10 -0400

    formatted html and css of clock app

--- javascript.js
+++
@@ -1,6 +0,0 @@
-var date = new Date
-var current_time = date.getHours() +":"+date.getMinutes()+":"+ date.getSeconds();
-var current_date = date.getFullYear()+"-"+(date.getMonth('')+1)+"-"+ date.getDate();
-var date_time = current_time+"<br>"+current_date;
-
-document.getElementById("p1").innerHTML = date_time; 
